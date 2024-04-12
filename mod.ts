4898ff6291a52f089bdaea9b4ee46d411b5f21c9
hugoalh <32359235+hugoalh@users.noreply.github.com>
2024-04-12 14:42:22 +0800

    Add subpath

--- mod.ts
+++ mod.ts
@@ -1,2 +1,2 @@
-export { combinationSet, permutationSet, type SetationSetOptions } from "./set.ts";
 export { combinationMatrix } from "./matrix.ts";
+export { combinationSet, permutationSet, type SetationSetOptions } from "./set.ts";
