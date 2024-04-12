3765d777200e168fdd4d6e0b348bf8ac831823fb
hugoalh <32359235+hugoalh@users.noreply.github.com>
2024-04-12 14:37:23 +0800

    Update test

--- matrix.test.ts
+++ matrix.test.ts
@@ -1,7 +1,31 @@
 import { assertEquals } from "STD/assert/assert_equals.ts";
 import { combinationMatrix } from "./matrix.ts";
-Deno.test("Combination 1", { permissions: "none" }, () => {
-	const result = Array.from(combinationMatrix({ foo: [1, 2, 3], bar: [4, 5, 6] }));
+Deno.test("Matrix 1", { permissions: "none" }, () => {
+	const matrix = {
+		foo: [1, 2, 3],
+		bar: [4, 5, 6]
+	};
+	const count = Object.values(matrix).map((value) => {
+		return value.length;
+	}).reduce((previous, current) => {
+		return previous * current;
+	});
+	const result = Array.from(combinationMatrix(matrix));
 	console.log(result);
-	assertEquals(result.length, 9);
+	assertEquals(result.length, count);
+});
+Deno.test("Matrix 2", { permissions: "none" }, () => {
+	const matrix = {
+		a: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+		b: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+		c: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+	};
+	const count = Object.values(matrix).map((value) => {
+		return value.length;
+	}).reduce((previous, current) => {
+		return previous * current;
+	});
+	const result = Array.from(combinationMatrix(matrix));
+	console.log(result);
+	assertEquals(result.length, count);
 });
--- set.test.ts
+++ set.test.ts
@@ -1,8 +1,8 @@
 import { assertEquals } from "STD/assert/assert_equals.ts";
 import { combinationSet, permutationSet } from "./set.ts";
-Deno.test("Combination 1", { permissions: "none" }, async (t) => {
+Deno.test("Set 1", { permissions: "none" }, async (t) => {
 	const set = ["a", "b", "c", "d", "e", "f"];
-	await t.step("AllowRepeat 3", () => {
+	await t.step("Combination AllowRepeat 3", () => {
 		const result = Array.from(combinationSet(set, {
 			allowRepeat: true,
 			size: 3
@@ -10,20 +10,17 @@ Deno.test("Combination 1", { permissions: "none" }, async (t) => {
 		console.log(result);
 		assertEquals(result.length, 56);
 	});
-	await t.step("NoRepeat 3", () => {
+	await t.step("Combination NoRepeat 3", () => {
 		const result = Array.from(combinationSet(set, { size: 3 }));
 		console.log(result);
 		assertEquals(result.length, 20);
 	});
-	await t.step("NoRepeat 6", () => {
+	await t.step("Combination NoRepeat 6", () => {
 		const result = Array.from(combinationSet(set, { size: 6 }));
 		console.log(result);
 		assertEquals(result.length, 1);
 	});
-});
-Deno.test("Permutation 1", { permissions: "none" }, async (t) => {
-	const set = ["a", "b", "c", "d", "e", "f"];
-	await t.step("AllowRepeat 3", () => {
+	await t.step("Permutation AllowRepeat 3", () => {
 		const result = Array.from(permutationSet(set, {
 			allowRepeat: true,
 			size: 3
@@ -31,12 +28,12 @@ Deno.test("Permutation 1", { permissions: "none" }, async (t) => {
 		console.log(result);
 		assertEquals(result.length, 216);
 	});
-	await t.step("NoRepeat 3", () => {
+	await t.step("Permutation NoRepeat 3", () => {
 		const result = Array.from(permutationSet(set, { size: 3 }));
 		console.log(result);
 		assertEquals(result.length, 120);
 	});
-	await t.step("NoRepeat 6", () => {
+	await t.step("Permutation NoRepeat 6", () => {
 		const result = Array.from(permutationSet(set, { size: 6 }));
 		console.log(result);
 		assertEquals(result.length, 720);
